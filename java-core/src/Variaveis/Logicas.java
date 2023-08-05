1e0d9751419688488f6e4432019a463f180b9838
tci-dev <tci-dev@192.168.66.181>
2023-08-04 21:47:57 -0300

    Atualizado

---
+++ java-core/src/Variaveis/Logicas.java
@@ -0,0 +1,16 @@
+package Variaveis;
+
+public class Logicas {
+
+	public static void main(String[] args) {
+		
+		int x = 5;
+		
+		System.out.println(x <=20 && x == 10);
+		System.out.println(x > 0 && x == 3);
+		System.out.println(x <=0 || x !=3 && x !=5);
+		
+
+	}
+
+}
